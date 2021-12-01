732d81655b39478ffcc9d73d4f77075bf2e8a20f
Darrin Speegle <speegled@slu.edu>
2021-12-01 16:45:40 -0600

    i'm a star!

--- R/day_01.R
+++ R/day_01.R
@@ -1,4 +1,4 @@
 dd <- read.csv(here::here("data/day_01"), header = F)
-sum(diff(dd$V1) > 0)
+sum(diff(dd$V1) > 0) #first star!
 
-sum(diff(filter(dd$V1, filter = c(1,1,1))) > 0, na.rm = T)
+sum(diff(filter(dd$V1, filter = c(1,1,1))) > 0, na.rm = T) #second star! 
